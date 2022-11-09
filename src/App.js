47a50049e0cdd4042eaa80086d5419d23bfd1ad9
Mauricio Molinari <mauricio.molinari@sicoob.com.br>
2022-11-09 12:06:44 -0300

    imported component to main page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Word</h1>
+        <Footer />
     </div>
   );
 }
